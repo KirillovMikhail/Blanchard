8ec3bab5abc5a2166e0b3a01e450cb3d1fcb8ccd
Kirillov Mikhail <kirillovmikhail88@gmail.com>
2022-07-26 00:51:44 +0300

    add header-list-inside

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
+let headerListBtn1 = document.querySelector('.header__list-btn--1');
+let headerListInside1 = document.querySelector('.header__inside--1');
+
+// headerListBtn.forEach(function(btn) {
+  headerListBtn1 .addEventListener('click',
+  function () {
+    headerListInside1.classList.toggle('is__active');
+    });
+// });
+
+let headerListBtn2 = document.querySelector('.header__list-btn--2');
+let headerListInside2 = document.querySelector('.header__inside--2');
+
+  headerListBtn2 .addEventListener('click',
+  function () {
+    headerListInside2.classList.toggle('is__active');
+    });
+
+let headerListBtn3 = document.querySelector('.header__list-btn--3');
+let headerListInside3 = document.querySelector('.header__inside--3');
+
+  headerListBtn3 .addEventListener('click',
+  function () {
+    headerListInside3.classList.toggle('is__active');
+    });
+
+let headerListBtn4 = document.querySelector('.header__list-btn--4');
+let headerListInside4 = document.querySelector('.header__inside--4');
+
+  headerListBtn4 .addEventListener('click',
+  function () {
+    headerListInside4.classList.toggle('is__active');
+    });
+
+let headerListBtn5 = document.querySelector('.header__list-btn--5');
+let headerListInside5 = document.querySelector('.header__inside--5');
+
+  headerListBtn5 .addEventListener('click',
+  function () {
+    headerListInside5.classList.toggle('is__active');
+    });
+
+// new SimpleBar(document.getElementById('header__list-inside'), {
+//   autoHide: false,
+//   scrollbarMaxSize: 70,
+//   forceVisible: true,
+// });
+
+
+
 const element = document.querySelector('.select');
 const choices = new Choices(element, {
    searchEnabled: false,
